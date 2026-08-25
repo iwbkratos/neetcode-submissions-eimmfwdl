e598d0015a61e380da12706f12f5e81b408d6342
Guna <145008540+iwbkratos@users.noreply.github.com>
2026-08-25 14:37:40 +0530

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int current = nums0;
+        int max = nums[0];
+
+        for(int i=0; i<nums.length; i++)
+        {
+            current += nums[i]; 
+            max = Math.max(current,max);
+            if(current < 0)
+            {
+                current = 0;
+            }
+        }
+
+        return max;
+
+    }
+}
