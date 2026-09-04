c369173a301cf6df260d570d2125d375d79dfde7
Guna <145008540+iwbkratos@users.noreply.github.com>
2026-09-04 16:11:25 +0530

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {        
+        int l = 0;
+        int r = nums.length-1;
+
+        while(l<r)
+        {
+            if(nums[l]==val)
+            {
+                nums[l] = nums[r];
+                r--;
+            } 
+            else
+            {
+                l++;
+            }          
+        }
+
+        return r+1;
+    }
+}
