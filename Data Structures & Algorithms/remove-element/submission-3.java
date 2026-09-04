4064401fa52efc8fcf2206ccc1845f9d0638f21b
Guna <145008540+iwbkratos@users.noreply.github.com>
2026-09-04 16:03:10 +0530

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int index = -1;
+        int l = 0;
+        int r = nums.length-1;
+
+        while(l<r)
+        {
+            while(nums[l]!=val && l<r)
+            {
+                l++;
+            }
+
+            while(nums[r]==val && l<r)
+            {
+                r--;
+            }
+
+            if(l<r)
+            {
+            int temp = nums[r];
+            nums[r]  = nums[l];
+            nums[l] = temp;
+            }
+            
+           
+        }
+
+        return l;
+    }
+}
