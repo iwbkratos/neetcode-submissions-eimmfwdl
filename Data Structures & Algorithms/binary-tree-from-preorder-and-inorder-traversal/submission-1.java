11987e73b670c83b1e36f4de14a7fc458fb23de3
Guna <145008540+iwbkratos@users.noreply.github.com>
2026-09-04 14:27:21 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+       int s1 =0;
+       int s2 = 0;
+       int e1 = preorder.length-1;
+       int e2 = inorder.length-1; 
+
+       return build(preorder,s1,e1,inorder,s2,e2);
+       
+    }
+
+    private TreeNode build(int[]p, int s1, int e1, int in[], int s2, int e2)
+    {  
+        // no preorder elements left to construct tree
+        if(s1 > e1)
+        {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(p[s1]);
+
+        int index = -1;
+//      find index of pre ele in inorder
+        for(int i=s2; i<=e2; i++)
+        {
+            if(p[s1] == in[i])
+            {
+                index = i;
+                break;
+            }
+        }
+//      conut for splitting arrays : lst & rst
+        int c = index-s2;
+
+//      lst 
+        root.left = build(p, s1+1, s1+c, in, s2, index-1);
+//      rst
+        root.right = build(p,s1+c+1, e1, in, index+1, e2);
+
+        return root;
+    }
+
+}
