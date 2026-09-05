f5bf2d7825cac6b76bd8f22d66172d9f16899071
Guna <145008540+iwbkratos@users.noreply.github.com>
2026-09-05 22:48:28 +0530

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+         return remove(root,target);
+    }
+
+    private TreeNode remove(TreeNode node, int target)
+    {
+        if(node == null)
+           return null;
+
+        node.left = remove(node.left,target);
+        node.right = remove(node.right,target);
+
+        if(node.val == target && node.right == null && node.left == null)
+        {
+            return null;
+        }
+
+        return node;
+    }
+}
