80dc8273e80a91be4c20a8fb8ee451c53f53cff8
Guna <145008540+iwbkratos@users.noreply.github.com>
2026-09-13 02:33:06 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        
+        int l = 0;
+        int r = s.length()-1;
+        int count = 0;
+        while(l<=r)
+        {
+            if(s.charAt(l) != s.charAt(r))
+            {
+                count++;
+            }
+            l++;
+            r--;
+        }
+
+        return count <= 1;
+    }
+}
